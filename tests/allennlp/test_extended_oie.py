6d5a1cab29bd529c0762b818aadc8d32b9d2abe8
schlevik <vtschlegel@gmail.com>
2020-04-14 11:53:36 +0100

    Add extended predicate detection for OIE
    * will consolidate things like "Like to bake" or "Decided to run" into one predicate now
    
    * tested, ready to merge

---
+++ tests/allennlp/test_extended_oie.py
@@ -0,0 +1,19 @@
+from tmdm.allennlp.oie import join_mwp
+
+
+def test_join_mwp_joins_predicates():
+    tags = "O B-ARG0 B-V O B-V B-ARG1 O".split()
+    mask = [0, 0, 1, 1, 1, 0, 0]
+    assert join_mwp(tags, mask) == "O B-ARG0 B-V I-V I-V B-ARG1 O".split()
+
+
+def test_join_mwp_join_predicates_at_beginning():
+    tags = "B-V O B-V B-ARG0 B-ARG1 O O".split()
+    mask = [1, 1, 1, 0, 0, 0, 0]
+    assert join_mwp(tags, mask) == "B-V I-V I-V B-ARG0 B-ARG1 O O".split()
+
+
+def test_join_mwp_sets_new_arg_boundries():
+    tags = "B-ARG0 B-V B-ARG1 I-ARG1 O O".split()
+    mask = [0, 1, 1, 0, 0, 0]
+    assert join_mwp(tags,mask) == "B-ARG0 B-V I-V B-ARG1 O O".split()
--- tmdm/allennlp/oie.py
+++ tmdm/allennlp/oie.py
@@ -1,18 +1,12 @@
-from functools import partial
-from operator import attrgetter, itemgetter
-
 from allennlp.common import JsonDict
 from allennlp.common.util import sanitize
 from allennlp.data import Instance
 from allennlp.predictors import Predictor
-from allennlp_models.syntax import SrlReader
-from allennlp_models.syntax.srl.openie_predictor import sanitize_label, consolidate_predictions, join_mwp, \
-    make_oie_string, get_predicate_text
+from allennlp_models.syntax.srl.openie_predictor import sanitize_label, consolidate_predictions
 from collections import defaultdict
 from loguru import logger
-from overrides import overrides
-from spacy.tokens import Doc, Token
-from typing import List, Tuple, Dict, Any
+from spacy.tokens import Doc, Token, Span
+from typing import List, Dict, Callable
 import numpy as np
 from tmdm.allennlp.common import OnlineProvider
 from tmdm.classes import OffsetAnnotation
@@ -24,17 +18,76 @@ from tmdm.util import entities_relations_from_by_verb
 ModelOutput = Dict[str, np.ndarray]
 
 
-def post_process(results: List[ModelOutput], sent_tokens: List[Token]):
+# def join_mwp(tags: List[str]) -> List[str]:
+#     """
+#     Join multi-word predicates to a single
+#     predicate ('V') token.
+#     """
+#     ret = []
+#     verb_flag = False
+#     for tag in tags:
+#         if "V" in tag:
+#             # Create a continuous 'V' BIO span
+#             prefix, _ = tag.split("-", 1)
+#             if verb_flag:
+#                 # Continue a verb label across the different predicate parts
+#                 prefix = "I"
+#             ret.append(f"{prefix}-V")
+#             verb_flag = True
+#         else:
+#             ret.append(tag)
+#             verb_flag = False
+#
+#     return ret
+
+
+def join_mwp(tags: List[str], mask: List[int]):
+    # assuming that the predicate is continuous
+    predicate_start = next(i for i, is_predicate in enumerate(mask) if is_predicate)
+    logger.trace(f"Predicate start: {predicate_start}")
+    predicate_length = sum(mask)
+    pre_verb = tags[:predicate_start]
+    verb = ['B-V'] + ["I-V"] * (predicate_length - 1)
+    one_past_verb = tags[predicate_start + predicate_length]
+    if one_past_verb.startswith("I"):
+        _, ann = one_past_verb.split("-")
+        new_one_past_verb = f"B-{ann}"
+    else:
+        new_one_past_verb = one_past_verb
+    rest = tags[predicate_start + predicate_length + 1:]
+    logger.trace(f"MWP joined: {pre_verb} + {verb} + {[new_one_past_verb]} + {rest}")
+    return pre_verb + verb + [new_one_past_verb] + rest
+
+
+def _extract_predicates(sent):
+    pred_ids = []
+    for i, t in enumerate(sent):
+        if t.pos_ == "VERB" or (t.pos_ == "PART" and i > 0 and sent[i - 1].pos_ == "VERB"):
+            pred_ids.append(i)
+    masks = [[0] * len(sent)]
+    masks[0][pred_ids[0]] = 1
+    for (prev_idx, idx) in zip(pred_ids, pred_ids[1:]):
+        # consecutive
+        if idx - prev_idx == 1:
+            masks[len(masks) - 1][idx] = 1
+        else:
+            mask = [[0] * len(sent)]
+            mask[idx] = 1
+            masks.append(mask)
+    return masks
+
+
+def post_process(results: List[ModelOutput], sent_tokens: List[Token], predicate_mask: List[int]):
     outputs: List[List[str]] = [[sanitize_label(label) for label in result['tags']] for result in results]
-
+    logger.trace(f"Outputs: {outputs}")
     pred_dict = consolidate_predictions(outputs, sent_tokens)
-
+    logger.trace(f"Consolidated predictions: {pred_dict}")
     # Build and return output dictionary
     final_results: JsonDict = {"verbs": []}
 
     for tags in pred_dict.values():
         # Join multi-word predicates
-        tags = join_mwp(tags)
+        tags = join_mwp(tags, predicate_mask)
 
         # Create description text
         # description = make_oie_string(sent_tokens, tags)
@@ -45,29 +98,43 @@ def post_process(results: List[ModelOutput], sent_tokens: List[Token]):
     return sanitize(final_results)
 
 
+def _extract_predicates_simple(sent: Span) -> List[List[int]]:
+    verb_indices = [i for (i, t)
+                    in enumerate(sent)
+                    if t.pos_ == "VERB"]
+    masks = [[0] * len(sent) for _ in verb_indices]
+
+    for i, mask in enumerate(masks):
+        mask[verb_indices[i]] = 1
+    return masks
+
+
 @Predictor.register("open-information-extraction", exist_ok=True)
 class CustomOpenIEPredictor(OpenIePredictor):
+    extract_predicates: Callable[[Span], List[List[int]]]
+
     def predict_batch_instance(self, instances: List[Instance]) -> List[JsonDict]:
         # noinspection PyTypeChecker
         return self.predict_batch_documents(instances)
 
     def predict_batch_documents(self, inputs: List[Doc]):
+        logger.trace(f"Docs: {inputs}")
         instances: Dict[Doc, List[List[Instance]]] = defaultdict(list)
         for d in inputs:
-            logger.trace(f"{d}")
+            logger.trace(f"Doc: {d}")
             # TODO: make smarter predicate selection
             for sent_nr, sentence in enumerate(d.sents):
-                logger.trace(f"{sent_nr}: {sentence}")
-                verb_indices = [i for (i, t)
-                                in enumerate(sentence)
-                                if t.pos_ == "VERB"]
-                masks = [[0] * len(sentence) for _ in verb_indices]
+                # logger.trace(f"{sent_nr}: {sentence}")
+                # verb_indices = [i for (i, t)
+                #                 in enumerate(sentence)
+                #                 if t.pos_ == "VERB"]
+                masks = self.extract_predicates(sentence)
 
-                for i, mask in enumerate(masks):
-                    mask[verb_indices[i]] = 1
+                # for i, mask in enumerate(masks):
+                #     mask[verb_indices[i]] = 1
                 logger.trace(f"masks: {masks}")
                 instances[d].append([self._dataset_reader.text_to_instance(sentence, mask) for mask in masks])
-        logger.debug(instances)
+        logger.trace(f"Instances: {instances}")
         # tokens.append(sentence)
         try:
             results = self._model.forward_on_instances(
@@ -76,15 +143,15 @@ class CustomOpenIEPredictor(OpenIePredictor):
             logger.error(str(e))
             return [[] for _ in inputs]
         results_iterator = iter(results)
+        masks_iter = iter(masks)
         # per doc/ per sent / per verb / per token
         gathered_results: List[List[List[List[str]]]] = list()
-        for document, sentences in instances.items():
+        for document, per_predicate in instances.items():
+            doc_sents = list(document.sents)
             per_doc = []
-            # gathered_results[document].append([])
-            for i, sentence in enumerate(sentences):
+            for i, sentence in enumerate(per_predicate):
                 r = [next(results_iterator) for _ in sentence]
-                # gathered_results[document][i].append(post_process(r, document._spacy_tokens[i])['verbs'])Cy
-                per_doc.append(post_process(r, list(document.sents)[i])['verbs'])
+                per_doc.append(post_process(r, doc_sents[i], next(masks_iter))['verbs'])
             gathered_results.append(per_doc)
         return gathered_results
 
@@ -102,20 +169,9 @@ def _convert_annotations(doc: Doc, annotations: List[List[List[str]]]) -> Offset
     return entities_relations_from_by_verb(result)
 
 
-# def _preprocess(dr: SrlReader, doc: Doc) -> List[Instance]:
-#     instances = []
-#     for sent in doc.sents:
-#         for i, verb in enumerate(t for t in sent if is_verb(doc, t)):
-#             tokens = [t for t in sent]
-#             verb_mask = [0] * len(sent)
-#             verb_mask[i] = 1
-#             instances.append(dr.text_to_instance(tokens, verb_mask))
-#     return instances
-
-
-def get_oie_provider(model_path: str):
-    from allennlp_models.coref.coref_model import CoreferenceResolver
-    converter = _convert_annotations
-    # getter = itemgetter("verbs")
-    p = OnlineProvider(task='open-information-extraction', path=model_path, converter=converter, preprocessor=None)
+def get_oie_provider(model_path: str, extract_predicates: Callable[[Span], List[List[int]]] = None):
+    extract_predicates = extract_predicates or _extract_predicates
+    p = OnlineProvider(task='open-information-extraction', path=model_path, converter=_convert_annotations,
+                       preprocessor=None)
+    p.predictor.extract_predicates = extract_predicates
     return p
