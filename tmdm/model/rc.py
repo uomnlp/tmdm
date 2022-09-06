53b4be5a751c40a277b4fd32e9860570dd51b936
schlevik <viktor@schlegel-online.de>
2022-09-06 17:57:32 +0900

    fix formatting

--- tmdm/model/rc.py
+++ tmdm/model/rc.py
@@ -10,7 +10,6 @@ from tmdm.classes import ERTuple
 from tmdm.model.extensions import Annotation, extend
 
 
-
 def set_relations(self: Doc, relations):
     if self._._relations:
         logger.error("Cannot re-set tokens (yet)!")
@@ -26,10 +25,10 @@ def relations(self: Doc):
 
     Relation.doc = self.doc
     # Two tuples in _relations for each Relation instance in relations
-    return  [Relation.make(ind) for ind in range(0,len(self._._relations),2)]
+    return [Relation.make(ind) for ind in range(0, len(self._._relations), 2)]
 
 
-class Relation():
+class Relation:
     doc: Doc
 
     def __init__(self, idx, relation_type):
@@ -38,21 +37,21 @@ class Relation():
 
     @property
     def subject(self):
-        ind = self.idx*2
+        ind = self.idx * 2
         (start, end, label) = self.doc._._relations[ind]
         return Annotation.make(self.doc, ind, start, end, label)
 
     @property
     def object(self):
-        ind = self.idx*2
-        (start, end, label) = self.doc._._relations[ind+1]
-        return Annotation.make(self.doc, ind+1, start, end, label)
+        ind = self.idx * 2
+        (start, end, label) = self.doc._._relations[ind + 1]
+        return Annotation.make(self.doc, ind + 1, start, end, label)
 
     @classmethod
     def make(cls, ind: int):
         # Subject and object at ind and ind+1 where ind = 2*idx
         (_, _, subjlabel) = Relation.doc._._relations[ind]
         relation_type = subjlabel.split("-")[-1]
-        relation = Relation(int(ind/2), relation_type)
+        relation = Relation(int(ind / 2), relation_type)
         logger.debug(f"{relation.subject} - {relation.relation_type} - {relation.object}")
         return relation
